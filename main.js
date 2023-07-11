3bb50a9dba9f8ecb063aff827bfcdb7cd0d23655
Parrot Man <100326980+Aternosus@users.noreply.github.com>
2023-07-11 09:23:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ async function init(){
     //Load liveries
 
 
-    await fetch("https://raw.githubusercontent.com/kolos26/GEOFS-LiverySelector/main/livery.json").then(res => res.json()).then(data => liveryobj = data)
+//    await fetch("https://raw.githubusercontent.com/kolos26/GEOFS-LiverySelector/main/livery.json").then(res => res.json()).then(data => liveryobj = data)
 
     //remove original buttons
 
